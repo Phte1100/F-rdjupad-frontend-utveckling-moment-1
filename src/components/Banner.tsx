a899d57138cd434f88191136e248bf2d426c2480
phte1100 <phte1100@student.miun.se>
2025-02-09 15:47:01 +0100

    Lagt till banner-bild

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,15 +1,20 @@
+import bannerImage from '/banner-image.jpg'; // Importera bilden
+
 // Den tar emot en prop "title" som är en sträng
 function Banner({ title }: { title: string }) {
   return (
     <div
       className="banner"
       style={{
-        backgroundColor: 'lightblue',
+        backgroundImage: `url('/banner-image.jpg')`, // Använd bilden som bakgrund
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
         width: '100%',
-        height: '100px',
+        height: 'auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        textShadow: '2px 2px 4px rgba(0,0,0,0.5)',
       }}
     >
       <h1>{title}</h1>
